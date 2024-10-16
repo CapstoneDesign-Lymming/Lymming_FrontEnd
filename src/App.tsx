a6d1752ad0e3b8f6bcaf132779341ae96a5e07f4
Junseo11 <0302ser@naver.com>
2024-10-17 00:45:47 +0900

    Feat: 메인페이지 구조 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Main from "./pages/Main/Main";
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
