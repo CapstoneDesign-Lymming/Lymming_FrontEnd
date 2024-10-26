5ee1bed5f39b6057281b58ab4e859cbd7c2f2a8c
Junseo11 <0302ser@naver.com>
2024-10-26 14:29:22 +0900

    Design: 사용자 정보 모달 퍼블리싱

--- src/pages/ParticipatePage/Participate.tsx
+++ src/pages/ParticipatePage/Participate.tsx
@@ -2,13 +2,23 @@ import Header from "../../components/header/Header";
 import "./Participate.scss";
 import dummy from "../../data/participateDummyData.json";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Usermodal from "../../components/Modal/UserModal/UserModal";
 
 const Participate = () => {
   const navigate = useNavigate();
+  const [userModalOpen, setUserModalOpen] = useState(true);
 
   return (
     <div className="Participate">
       <Header />
+
+      {userModalOpen && (
+        <>
+          <div className="backdrop" />
+          <Usermodal />
+        </>
+      )}
 
       <div className="menu">
         <div className="menu-tap">
@@ -26,34 +36,38 @@ const Participate = () => {
       <div className="content">
         {dummy.dummy.map((it, index) => {
           return (
-            <div
-              className="content-item"
-              key={index}
-              onClick={() => navigate(`/participate/detail/${index}`)}
-            >
-              <div className="content-item-label">
-                <div className="content-item-label-left">프로젝트</div>
-                <div className="content-item-label-right">
-                  <img />
-                  <span>D-20</span>
+            <div className="content-item" key={index}>
+              <div
+                className="content-item-top"
+                onClick={() => navigate(`/participate/detail/${index}`)}
+              >
+                <div className="content-item-top-label">
+                  <div className="content-item-top-label-left">프로젝트</div>
+                  <div className="content-item-top-label-right">
+                    <img />
+                    <span>D-20</span>
+                  </div>
                 </div>
+                <div className="content-item-top-title">
+                  프로젝트를 쉽게 구하는 플랫폼 리밍의 팀원을 모집합니다.
+                </div>
+                <div className="content-item-top-info">
+                  <span>마감</span> <span>|</span> <span>2024.11.07</span>
+                </div>
+                <div className="content-item-top-feature">
+                  <span>프론트</span> <span>백엔드</span> <span>디자이너</span>
+                </div>
+                <div className="content-item-top-skills">
+                  <img />
+                  <img />
+                  <img />
+                </div>
+                <hr />
               </div>
-              <div className="content-item-title">
-                프로젝트를 쉽게 구하는 플랫폼 리밍의 팀원을 모집합니다.
-              </div>
-              <div className="content-item-info">
-                <span>마감</span> <span>|</span> <span>2024.11.07</span>
-              </div>
-              <div className="content-item-feature">
-                <span>프론트</span> <span>백엔드</span> <span>디자이너</span>
-              </div>
-              <div className="content-item-skills">
-                <img />
-                <img />
-                <img />
-              </div>
-              <hr />
-              <div className="content-item-bottom">
+              <div
+                className="content-item-bottom"
+                onClick={() => setUserModalOpen(true)}
+              >
                 <div className="content-item-bottom-left">
                   <img />
                   <span>프론트짱</span>
