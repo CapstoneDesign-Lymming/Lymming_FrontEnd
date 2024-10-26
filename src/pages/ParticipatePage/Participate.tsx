1f59fc7ab9ae675cd185bbe13b5245f684d70fb3
Junseo11 <0302ser@naver.com>
2024-10-26 20:58:02 +0900

    Feat: 프로젝트 페이지 필터 추가

--- src/pages/ParticipatePage/Participate.tsx
+++ src/pages/ParticipatePage/Participate.tsx
@@ -4,10 +4,48 @@ import dummy from "../../data/participateDummyData.json";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Usermodal from "../../components/Modal/UserModal/UserModal";
+import skill_data from "../../data/skills.json";
+
+interface ParticipateItem {
+  type: string;
+  title: string;
+  end: string;
+  position: string[];
+  style: string[];
+  name: string;
+}
 
 const Participate = () => {
   const navigate = useNavigate();
   const [userModalOpen, setUserModalOpen] = useState(false);
+
+  const [selectTab, setSelectTab] = useState("전체");
+  const [data, setData] = useState<ParticipateItem[]>([]);
+
+  const [visible, setVisible] = useState(false);
+  const [selectedSkills, setSelectedSkills] = useState<string[]>([]);
+
+  const onSkillClick = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = e.target;
+
+    if (checked) {
+      // 체크된 경우 배열에 추가
+      setSelectedSkills((prev) => [...prev, value]);
+    } else {
+      // 체크 해제된 경우 배열에서 제거
+      setSelectedSkills((prev) => prev.filter((skill) => skill !== value));
+    }
+  };
+  useEffect(() => {
+    const filteredData =
+      selectTab === "전체"
+        ? dummy.dummy
+        : dummy.dummy.filter((it) => it.type === selectTab);
+
+    // 선택된 기술 스택으로 추가 필터링 로직추가
+
+    setData(filteredData);
+  }, [selectTab]);
 
   //서버에서 리스트 받아오는 걸로 수정해야함
 
@@ -21,22 +59,79 @@ const Participate = () => {
           <Usermodal close={setUserModalOpen} />
         </>
       )}
+      {visible && (
+        <div className="skillSelector">
+          <button onClick={() => setVisible(false)}>닫기</button>
+
+          <div className="skill-item">
+            {skill_data.skills.map((item) => (
+              <div key={item.name} className="wrapper">
+                <img src={item.url} />
+                <input
+                  type="checkbox"
+                  id={item.name}
+                  value={item.name}
+                  onChange={onSkillClick}
+                />
+                <label htmlFor={item.name} className="skill_name">
+                  {item.name}
+                </label>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
 
       <div className="menu">
         <div className="menu-tap">
-          <span>전체</span>
-          <span>프로젝트</span>
-          <span>스터디</span>
+          <span
+            className={selectTab === "전체" ? "active" : ""}
+            onClick={() => setSelectTab("전체")}
+          >
+            전체
+          </span>
+          <span
+            className={selectTab === "프로젝트" ? "active" : ""}
+            onClick={() => setSelectTab("프로젝트")}
+          >
+            프로젝트
+          </span>
+          <span
+            className={selectTab === "스터디" ? "active" : ""}
+            onClick={() => setSelectTab("스터디")}
+          >
+            스터디
+          </span>
         </div>
         <div className="menu-filter">
-          <span>기술스택</span>
-          <span>포지션</span>
-          <span>진행방식</span>
+          <span id="technology-stack" onClick={() => setVisible(!visible)}>
+            기술스택
+          </span>
+
+          <div>
+            <select id="position">
+              <option value="position" disabled hidden>
+                포지션
+              </option>
+              <option value="developer">개발자</option>
+              <option value="designer">디자이너</option>
+            </select>
+          </div>
+
+          <div>
+            <select id="method">
+              <option value="method" disabled hidden>
+                진행방식
+              </option>
+              <option value="agile">온라인</option>
+              <option value="waterfall">오프라인</option>
+            </select>
+          </div>
         </div>
       </div>
 
       <div className="content">
-        {dummy.dummy.map((it, index) => {
+        {data.map((it, index) => {
           return (
             <div className="content-item" key={index}>
               <div
