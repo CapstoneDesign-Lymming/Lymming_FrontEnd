63a692be5da370110527528283c75317a033a12f
gihoon <kiki01111@naver.com>
2024-11-02 01:32:46 +0900

    Feat: SharePage 퍼블리싱 작업

--- src/pages/SharePage/SharePage.tsx
+++ src/pages/SharePage/SharePage.tsx
@@ -9,12 +9,12 @@ interface itemType{
   project_url:string,
   project_description:string,
   team_member:number[],
-  team_member_name:string[]
-
+  team_member_name:string[],
+  is_completed:boolean,
 }
 
 const fetchLocalData  =async()=>{
-  const response = await fetch('/json/testJson.json');
+  const response = await fetch('/json/SharePage.json');
   if(!response.ok){
      throw new Error('Network error');
   }
@@ -30,13 +30,27 @@ const SharePage = () => {
         <Header/>
         <div className='SharePageWrapper'>
             <div className='SharePage'>
-                <div className='SharePage-Header'>팀 공유 페이지</div>
                 <div className='SharePage-BodyWrapper'>
                   {data.ShareData.map((item:itemType)=>(
-                    <div key={item.project_id}>
-                      <h2>{item.project_name}</h2>
-                      <p>{item.project_description}</p>
-                      <p>팀원: {item.team_member_name.join(", ")}</p>
+                    <div className={`SharePage-BodyWrapper-CardWrapper ${item.is_completed?"completed":""}`}
+                      key={item.project_id}>
+                        <div className='CardInsideWrapper'>
+                          <div className='CardHeader'>
+                            <div className='CardHeader-Title'>{item.project_name}</div>
+                          </div>
+                          <div className='CardBody'>
+                              <img src={`${item.project_url}`} alt="" />
+                          </div>
+                          <div className='CardFooter'>
+                            <div className='CardFooter-Description'>{item.project_description}</div>
+                            <div className='CardFooter-MembersWrapper'>
+                              {item.team_member_name.map((name)=>(
+                                <div className='memberItem'>{name}</div>
+                              ))}
+                            </div>
+                          </div>
+                        </div>
+                    
                     </div>
                   ))}
                 </div>
