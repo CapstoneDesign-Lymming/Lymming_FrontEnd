19cb665a0255818670ac028e1311a76359432eae
Junseo11 <0302ser@naver.com>
2024-11-03 20:05:41 +0900

    Feat: 채팅방 생성시 아이디 정렬기능 추가

--- src/components/ShareDetailComponent/ShareDetailLeader.tsx
+++ src/components/ShareDetailComponent/ShareDetailLeader.tsx
@@ -1,23 +1,23 @@
-interface ShareDetailLeaderProps{
-    leaderData: {
-        userId: number,
-        project_id: number,
-        project_name: string,
-        project_url: string,
-        project_description: string,
-        team_member: number[],
-        team_member_name: string[],
-        is_completed: boolean,
-      }
-  }
-
-const ShareDetailLeader = ({leaderData}:ShareDetailLeaderProps) => {
-    return (
-        // <div>{leaderData.project_description}</div>
-        <>
-            <div className="ShareDetailLeaderWrapper"></div>
-        </>
-    )
+interface ShareDetailLeaderProps {
+  leaderData: {
+    userId: number;
+    project_id: number;
+    project_name: string;
+    project_url: string;
+    project_description: string;
+    team_member: number[];
+    team_member_name: string[];
+    is_completed: boolean;
+  };
 }
 
-export default ShareDetailLeader
+const ShareDetailLeader = ({ leaderData }: ShareDetailLeaderProps) => {
+  return (
+    // <div>{leaderData.project_description}</div>
+    <>
+      <div className="ShareDetailLeaderWrapper"></div>
+    </>
+  );
+};
+
+export default ShareDetailLeader;
--- src/pages/ChatPage/ChatPage.tsx
+++ src/pages/ChatPage/ChatPage.tsx
@@ -81,10 +81,10 @@ const ChatPage = () => {
   // 채팅방이 있는지 검사
   const isExistChatRoom = async () => {
     if (partner) {
-      console.log("채팅방 조사");
+      const roomId = await sortChatRoomId(currentUser, partner);
       try {
         const res = await axios.post("http://localhost:8080/chat/existroom", {
-          roomId: `${currentUser}_${partner}`,
+          roomId: roomId,
         });
 
         console.log(res.data.type);
@@ -107,8 +107,9 @@ const ChatPage = () => {
 
   const createChatRoom = async () => {
     if (partner) {
+      const roomId = await sortChatRoomId(currentUser, partner);
       const payload = {
-        roomId: `${currentUser}_${partner}`,
+        roomId: roomId,
         userId1: currentUser,
         userId2: parterId,
       };
@@ -185,6 +186,14 @@ const ChatPage = () => {
       setInputMessage("");
       console.log(inputMessage);
     }
+  };
+
+  const sortChatRoomId = (userId1: String, userId2: String): String => {
+    let arr = [userId1, userId2];
+
+    arr.sort();
+
+    return `${arr[0]}_${arr[1]}`;
   };
 
   // 채팅방 목록 불러오기
