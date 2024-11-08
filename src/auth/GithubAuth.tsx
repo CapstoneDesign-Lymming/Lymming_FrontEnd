f2c2c361cb17879f5c136572a90fc73912fb57c2
Junseo11 <0302ser@naver.com>
2024-11-09 04:23:54 +0900

    Fix 깃허브 로그인 추가

--- src/auth/GithubAuth.tsx
+++ src/auth/GithubAuth.tsx
@@ -20,7 +20,7 @@ const GithubAuth = () => {
     console.log("로그인", code);
     try {
       const result = await axios.post(
-        "https://lymming-back.link/api/login/oauth2/redirect/github",
+        "https://lymming.link/api/login/oauth2/redirect/github",
         {
           code,
         }
