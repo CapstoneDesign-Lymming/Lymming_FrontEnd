e7a171bb26a6385ca7d8af495ebb8e43eebc00ea
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2024-11-12 18:54:28 +0900

    Refactor: useImgUpload2S3 Hook 생성

--- src/components/ShareDetailComponent/ShareDetailLeader.tsx
+++ src/components/ShareDetailComponent/ShareDetailLeader.tsx
@@ -4,8 +4,8 @@ import { useState } from "react";
 import Header from "../header/Header";
 import RootModal from "../Modal/RootModal/RootModal";
 import nouserImage from "../../assets/img/noimage.jpg";
-// import { useInfoStore } from "../../store/useLoginStore";
-import AWS from "aws-sdk";
+import useImgUpload2S3 from "../../hooks/useImgUpload2S3";
+// import AWS from "aws-sdk";
 import "./ShareDetailLeader.scss";
 
 interface ShareDetailLeaderProps {
@@ -29,10 +29,12 @@ const ShareDetailLeader = () => {
   const [modalName, setModalName] = useState("");
   const [formData, setFormData] = useState<ShareDetailLeaderProps>(initialData);
   const [projectLink, setProjectLink] = useState("");
+  const { imageUrl, handleFileChange, handleUpload } = useImgUpload2S3();
   // const [image, setImage] = useState<string | null>(null); // 이미지 URL을 저장할 상태
   // const { setData } = useInfoStore();
-  const [selectedFile, setSelectedFile] = useState<File | null>(null); //FIXME: 추가됨
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  //------------------------------------------
+  // const [selectedFile, setSelectedFile] = useState<File | null>(null); //FIXME: 추가됨
+  // const [imageUrl, setImageUrl] = useState<string | null>(null);
   /** 입력 값 변경 핸들러 */
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -62,48 +64,50 @@ const ShareDetailLeader = () => {
   //     setData({ userImg: imageUrl });
   //   }
   // };
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0] || null;
-    if (file) {
-      setSelectedFile(file);
-      const objectUrl = URL.createObjectURL(file);
-      setImageUrl(objectUrl);
-      console.log("⭐미리보기:", imageUrl);
-    }
-  };
-  const handleUpload = () => {
-    if (!selectedFile) {
-      alert("파일을 선택해주세요");
-      return;
-    }
-    console.log(selectedFile);
-    AWS.config.update({
-      accessKeyId: import.meta.env.VITE_SECRET_KEY,
-      secretAccessKey: import.meta.env.VITE_SECRET_ACCESS_KEY,
-      region: "ap-northeast-2",
-    });
 
-    const s3 = new AWS.S3();
-    console.log(selectedFile);
-    const uploadParams = {
-      Bucket: import.meta.env.VITE_IMG_S3,
-      Key: `folder${selectedFile.name}`, // S3에 저장될 경로와 파일명
-      Body: selectedFile,
-    };
-    s3.upload(
-      uploadParams,
-      (err: unknown, data: { ETag: unknown; Location: unknown }) => {
-        if (err) {
-          console.error("Error uploading file:", err);
-        } else {
-          console.log("File uploaded successfully. ETag:", data.ETag);
-          const uploadedFileUrl = data.Location;
-          console.log(uploadedFileUrl);
-          //TODO: uploadedFileUrl은 s3에 저장된 이미지의 url로 백엔드에게 전달해주기
-        }
-      }
-    );
-  };
+  //------------------------------------------
+  // const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const file = e.target.files?.[0] || null;
+  //   if (file) {
+  //     setSelectedFile(file);
+  //     const objectUrl = URL.createObjectURL(file);
+  //     setImageUrl(objectUrl);
+  //     console.log("⭐미리보기:", imageUrl);
+  //   }
+  // };
+  // const handleUpload = () => {
+  //   if (!selectedFile) {
+  //     alert("파일을 선택해주세요");
+  //     return;
+  //   }
+  //   console.log(selectedFile);
+  //   AWS.config.update({
+  //     accessKeyId: import.meta.env.VITE_SECRET_KEY,
+  //     secretAccessKey: import.meta.env.VITE_SECRET_ACCESS_KEY,
+  //     region: "ap-northeast-2",
+  //   });
+
+  //   const s3 = new AWS.S3();
+  //   console.log(selectedFile);
+  //   const uploadParams = {
+  //     Bucket: import.meta.env.VITE_IMG_S3,
+  //     Key: `folder${selectedFile.name}`, // S3에 저장될 경로와 파일명
+  //     Body: selectedFile,
+  //   };
+  //   s3.upload(
+  //     uploadParams,
+  //     (err: unknown, data: { ETag: unknown; Location: unknown }) => {
+  //       if (err) {
+  //         console.error("Error uploading file:", err);
+  //       } else {
+  //         console.log("File uploaded successfully. ETag:", data.ETag);
+  //         const uploadedFileUrl = data.Location;
+  //         console.log(uploadedFileUrl);
+  //         //TODO: uploadedFileUrl은 s3에 저장된 이미지의 url로 백엔드에게 전달해주기
+  //       }
+  //     }
+  //   );
+  // };
   return (
     <>
       <Header />
