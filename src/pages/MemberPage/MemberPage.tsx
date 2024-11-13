54dd56a66dcb6ae970b791c14977d11482018fe7
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2024-11-14 01:30:34 +0900

    Design: Memberpage 수정

--- src/pages/MemberPage/MemberPage.tsx
+++ src/pages/MemberPage/MemberPage.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import Header from "../../components/header/Header";
 import { useInfoStore } from "../../store/useLoginStore";
 import "./MemberPage.scss";
 import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
 
 interface itemType {
   name: string;
@@ -21,11 +23,23 @@ const fetchLocalData = async () => {
 };
 
 const MemberPage = () => {
+  const navigate = useNavigate();
   const { data: userData } = useInfoStore();
+  const [flippedCardIndex, setFlippedCardIndex] = useState<number | null>(null); // 현재 뒤집힌 카드의 인덱스를 관리
   const nickname = userData.nickname;
+
+  const handleClickCard = (index: number) => {
+    setFlippedCardIndex(index === flippedCardIndex ? null : index); // 이미 뒤집힌 카드 클릭시 원상복귀
+  };
+  const handleChatClick = (e: React.MouseEvent, nickname: string) => {
+    e.stopPropagation(); // 이벤트 전파를 막아서 카드 뒤집기 동작을 막습니다.
+    console.log("채팅하기 클릭됨");
+    navigate("/chat", { state: { id: nickname } });
+  };
   const { data, error, isLoading } = useQuery("recommendData", fetchLocalData);
   if (isLoading) return <div>로딩 중!</div>;
   if (error) return <div>에러</div>;
+
   return (
     <>
       <Header />
@@ -37,16 +51,29 @@ const MemberPage = () => {
             </div>
 
             <div className="Member-header-recommend">
-              {data.recommendData.map((item: itemType) => (
-                <div className="recommendCard">
+              {data.recommendData.map((item: itemType, index: number) => (
+                <div
+                  key={item.name}
+                  className={`recommendCard ${
+                    flippedCardIndex === index ? "flipped" : ""
+                  }`}
+                  onClick={() => handleClickCard(index)}
+                >
                   <div className="front">
-                    <div id="recommend_name">{item.name}</div>
-                    <div id="recommend_position">{item.position}</div>
+                    <div className="recommend_name">{item.name}</div>
+                    <div className="recommend_position">{item.position}</div>
                     <img src={`${item.userImg}`} alt="" />
                   </div>
                   <div className="back">
-                    <div id="recommend_name">{item.name}</div>
-                    <div id="recommend_position">{item.position}</div>
+                    <div className="recommend_name">{item.name}</div>
+                    <div className="recommend_position">{item.position}</div>
+                    <div className="back_body"></div>
+                    <div
+                      className="back_footerWrapper"
+                      onClick={(e) => handleChatClick(e, item.name)}
+                    >
+                      <div className="back_footer">채팅하기</div>
+                    </div>
                   </div>
                 </div>
               ))}
