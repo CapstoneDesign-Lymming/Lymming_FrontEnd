2936a8a72e9022b94f9ae47a876e4ee8bc743a1a
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2024-11-14 04:11:29 +0900

    Design: MemberPagem not login ui세팅

--- src/pages/MemberPage/MemberPage.tsx
+++ src/pages/MemberPage/MemberPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Header from "../../components/header/Header";
-import { useInfoStore } from "../../store/useLoginStore";
+import { useInfoStore, useLoginStore } from "../../store/useLoginStore";
 import "./MemberPage.scss";
 import { useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
@@ -27,17 +27,23 @@ const fetchLocalData = async () => {
 const MemberPage = () => {
   const navigate = useNavigate();
   const { data: userData } = useInfoStore();
+  const { login } = useLoginStore();
+
   const [flippedCardIndex, setFlippedCardIndex] = useState<number | null>(null); // 현재 뒤집힌 카드의 인덱스를 관리
   const nickname = userData.nickname;
+
+  console.log("login여부", login);
 
   const handleClickCard = (index: number) => {
     setFlippedCardIndex(index === flippedCardIndex ? null : index); // 이미 뒤집힌 카드 클릭시 원상복귀
   };
+
   const handleChatClick = (e: React.MouseEvent, nickname: string) => {
     e.stopPropagation(); // 이벤트 전파를 막아서 카드 뒤집기 동작을 막습니다.
     console.log("채팅하기 클릭됨");
     navigate("/chat", { state: { id: nickname } });
   };
+
   const { data, error, isLoading } = useQuery("recommendData", fetchLocalData);
   if (isLoading) return <div>로딩 중!</div>;
   if (error) return <div>에러</div>;
@@ -47,7 +53,9 @@ const MemberPage = () => {
       <Header />
       <div className="MemberWrapper">
         <div className="Member">
-          <div className="Member-header">
+          <div
+            className={`Member-header ${login ? "" : "notLogin_MemberHeader"}`}
+          >
             <div className="Member-header-text">
               {nickname}님께 어울리는 사람을 추천해드립니다.
             </div>
@@ -104,6 +112,21 @@ const MemberPage = () => {
               ))}
             </div>
           </div>
+          <div className={`${login ? "hide_notLogin_nav" : "notLogin_nav"}`}>
+            <div className="notLogin_nav-head">
+              당신에게 어울리는 팀원을 찾아드립니다
+            </div>
+            <div className="notLogin_nav-body">
+              로그인 이후에 추천 기능을 이용해보세요
+            </div>
+            <div
+              className="notLogin_nav-foot"
+              onClick={() => navigate("/login")}
+            >
+              로그인하고 바로보기
+            </div>
+          </div>
+
           <div className="Member-body"></div>
         </div>
       </div>
