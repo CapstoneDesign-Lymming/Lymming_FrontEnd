a9b84eb20108687945f18380501e7bf16f0700b8
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2024-11-18 21:44:34 +0900

    Feat: memberpage 비로그인 시 toast, errorToast | successToast 메세지 세팅 (useToastStore)

--- src/store/useToastState.ts
+++ src/store/useToastState.ts
@@ -1,13 +1,23 @@
 import { create } from "zustand";
+
 interface ToastStore {
   isToastOpen: boolean;
   toastName: string;
+  successText: string;
+  errorText: string;
   openToast: () => void;
   closeToast: () => void;
+  setErrorText: (str: string) => void;
+  setSuccessText: (str: string) => void;
 }
+
 export const useToastStore = create<ToastStore>((set) => ({
   isToastOpen: false,
   toastName: "",
+  successText: "",
+  errorText: "",
   openToast: () => set({ isToastOpen: true }),
   closeToast: () => set({ isToastOpen: false }),
+  setSuccessText: (str) => set({ successText: str }),
+  setErrorText: (str) => set({ errorText: str }),
 }));
