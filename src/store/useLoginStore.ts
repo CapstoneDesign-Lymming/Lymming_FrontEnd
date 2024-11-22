ff9e69a51592ec208b2da44ebfde5daa3f3d996d
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2024-11-23 02:16:47 +0900

    Fix: useLoginStore

--- src/store/useLoginStore.ts
+++ src/store/useLoginStore.ts
@@ -27,7 +27,7 @@ interface InfoState {
 export const useLoginStore = create<LoginState>()(
   persist(
     (set) => ({
-      login: true,
+      login: false,
       count: 1,
       isOpen: false,
       //닉네임 중복체크
@@ -59,7 +59,7 @@ export const useInfoStore = create<InfoState>()(
         serverNickname: "",
         job: "",
         loginType: "",
-        nickname: "호이기훈",
+        nickname: "",
         position: "",
         stack: "",
         temperature: 0,
