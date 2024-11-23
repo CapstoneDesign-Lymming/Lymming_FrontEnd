c7010a1d8ba7ac7b5d16428a4a1b7ae9c4bc109f
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2024-11-23 19:40:49 +0900

    Feat: mypage 수정

--- src/pages/Mypage/Mypage.tsx
+++ src/pages/Mypage/Mypage.tsx
@@ -6,7 +6,7 @@ import "./Mypage.scss";
 import Header from "../../components/header/Header";
 import RootToast from "../../components/Toast/RootToast/RootToast";
 import no_profile from "../../assets/img/no-profile.webp";
-// import infoData from "../../data/loginInfoData.json";
+import infoData from "../../data/loginInfoData.json";
 interface putDataTypes {
   temperature: number;
   nickname: string;
@@ -22,6 +22,7 @@ const Mypage = () => {
   const { isToastOpen, openToast, setErrorText } = useToastStore();
   const [toastName, setToastName] = useState("");
   const [isOpenSelectBox, setIsOpenSelectBox] = useState(false);
+  const [isOpenStackBox, setIsOpenStackBox] = useState(false); // 기술 선택 박스 상태 추가
   const [putData, setPutDat] = useState<putDataTypes>({
     temperature: 0,
     nickname: "",
@@ -76,7 +77,28 @@ const Mypage = () => {
       [name]: value,
     }));
   };
-
+  const addOrRemoveStack = (stack: string) => {
+    const stackArr = putData.stack?.split(",").filter((item) => item); // 현재 스택 배열
+    console.log("stackArr", stackArr);
+    if (stackArr.includes(stack)) {
+      // 이미 존재하면 제거
+      const updatedStack = stackArr.filter((item) => item !== stack).join(",");
+      setPutDat((prevData) => ({
+        ...prevData,
+        stack: updatedStack,
+      }));
+    } else {
+      // 존재하지 않으면 추가
+      const updatedStack =
+        stackArr.length > 0 ? `${putData.stack},${stack}` : stack;
+      setPutDat((prevData) => ({
+        ...prevData,
+        stack: updatedStack,
+      }));
+      console.log("stackArr222", stackArr);
+    }
+    setIsOpenStackBox(false); // 선택 박스 닫기
+  };
   return (
     <>
       <Header />
@@ -104,40 +126,66 @@ const Mypage = () => {
               value={putData.nickname}
             ></input>
             <div className="input_text">포지션</div>
-            {putData.position && (
-              <div className="tagBundle">
-                <div className="tag">{putData.position}</div>
-                <div className="delete"> - </div>
-                <div className="plus" onClick={() => setIsOpenSelectBox(true)}>
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                    <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" />
-                  </svg>
-                </div>
-                <div className={`selectBox ${isOpenSelectBox ? "open" : ""}`}>
-                  <div
-                    className="closeBtn"
-                    onClick={() => setIsOpenSelectBox(false)}
-                  >
-                    닫기
-                  </div>
-                  <div></div>
-                </div>
-              </div>
-            )}
-            {/* <div>선택하기</div> */}
-            <div className="input_text">직업</div>
             <div className="tagBundle">
-              <div className="tag">{putData.job}</div>
-              <div className="delete"> - </div>
-
-              <div className="plus">
+              <div className="tag">{putData.position}</div>
+              {/* <div className="delete"> - </div> */}
+              <div className="plus" onClick={() => setIsOpenSelectBox(true)}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                   <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" />
                 </svg>
               </div>
+              <div className={`selectBox ${isOpenSelectBox ? "open" : ""}`}>
+                <div className="closeBtnWrapper">
+                  <svg
+                    className="closeBtn"
+                    onClick={() => setIsOpenSelectBox(false)}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  >
+                    <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" />
+                  </svg>
+                </div>
+                <div className="positonBundle">
+                  {infoData.position.map((item, idx) => (
+                    <div
+                      className={`newPosition ${
+                        putData.position === item ? "selectPosition" : ""
+                      }`}
+                      key={idx}
+                      onClick={() =>
+                        setPutDat((prevData) => ({
+                          ...prevData,
+                          position: item, // position 값 직접 변경
+                        }))
+                      }
+                    >
+                      {item}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="input_text">직업</div>
+            <div className="tagBundle">
+              <select
+                className="jobSelect"
+                value={putData.job}
+                onChange={(e) =>
+                  setPutDat((prevData) => ({
+                    ...prevData,
+                    job: e.target.value, // 선택된 값으로 job 업데이트
+                  }))
+                }
+              >
+                <option value="">직업 선택</option> {/* 기본 선택 */}
+                <option value="학생">학생</option>
+                <option value="직장인">직장인</option>
+                <option value="기타">기타</option>
+              </select>
             </div>
             {/* 학생 직장인 기타 */}
             <div className="input_text">기술</div>
+
             <div className="tagBundle">
               <div className="stackWrapper">
                 {stackArr.map((item, idx) => (
@@ -146,10 +194,35 @@ const Mypage = () => {
                   </div>
                 ))}
               </div>
-              <div className="plus">
+              <div className="plus" onClick={() => setIsOpenStackBox(true)}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                   <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" />
                 </svg>
+              </div>
+              <div className={`selectBox ${isOpenStackBox ? "open" : ""}`}>
+                <div className="closeBtnWrapper">
+                  <svg
+                    className="closeBtn"
+                    onClick={() => setIsOpenStackBox(false)}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  >
+                    <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4-9.4-24.6-9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" />
+                  </svg>
+                </div>
+                <div className="stackBundle">
+                  {infoData.skills.map((item, idx) => (
+                    <div
+                      className={`newStack ${
+                        stackArr.includes(item) ? "selected" : ""
+                      }`}
+                      key={idx}
+                      onClick={() => addOrRemoveStack(item)}
+                    >
+                      {item}
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
