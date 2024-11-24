569da45a50fe0b326de88104a080e3d0bb6e7c2e
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2024-11-24 16:37:52 +0900

    Design: mypage ui update

--- src/store/useLoginStore.ts
+++ src/store/useLoginStore.ts
@@ -27,7 +27,7 @@ interface InfoState {
 export const useLoginStore = create<LoginState>()(
   persist(
     (set) => ({
-      login: false,
+      login: true,
       count: 1,
       isOpen: false,
       isExist: false,
@@ -58,10 +58,10 @@ export const useInfoStore = create<InfoState>()(
         serverNickname: "",
         job: "",
         loginType: "",
-        nickname: "",
+        nickname: "호이루키",
         position: "",
         stack: "",
-        temperature: 0,
+        temperature: 44.4,
         userImg: "",
         keyCode: "",
         uid: 0,
