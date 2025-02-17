436c88f0b7ef3476a01391af4fd336a1cb5bd907
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-02-18 00:07:58 +0900

    Refactor: 성능최적화 [Suspense + lazy]

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
+import Loading from "./components/Loading/Loading";
 
 const Main = lazy(() => import("./pages/MainPage/Main"));
 const LogIn = lazy(() => import("./pages/LogInPage/LogIn"));
@@ -25,43 +26,44 @@ const ShareDetailLeader = lazy(
 const CollectPage = lazy(() => import("./pages/CollectPage/CollectPage"));
 const GithubAuth = lazy(() => import("./auth/GithubAuth"));
 const Mypage = lazy(() => import("./pages/Mypage/Mypage"));
-const Loading = lazy(() => import("./components/Loading/Loading"));
 const SeoMetaTag = lazy(() => import("./components/Helmet/SeoMetaTag"));
 
 function App() {
   return (
     <div className="App">
-      <SeoMetaTag
-        title="lymming"
-        description="리밍 | 프로젝트를 시작하기 위한 첫 걸음"
-      />
-      <Routes>
-        <Route path="/" element={<Main />}></Route>
-        <Route path="/login" element={<LogIn />}></Route>
-        <Route path="/auth" element={<KakaoAuth />}></Route>
-        <Route path="/api/callback" element={<GithubAuth />}></Route>
-        <Route
-          path="/videochat/:roomId"
-          element={<VideoChattingPage />}
-        ></Route>
-        <Route path="/participate" element={<Participate />}></Route>
-        <Route
-          path="/participate/detail/:projectId"
-          element={<ParticipateDetail />}
-        ></Route>
-        <Route path="/teambuild" element={<TeamBuilding />}></Route>
-        <Route path="/chat" element={<ChatPage />}></Route>
-        <Route path="/member" element={<MemberPage />}></Route>
-        <Route path="/share" element={<SharePage />}></Route>
-        <Route path="/share/detail/:id" element={<ShareDetailPage />}></Route>
-        <Route
-          path="/share/detail/leader"
-          element={<ShareDetailLeader />}
-        ></Route>
-        <Route path="/collect" element={<CollectPage />}></Route>
-        <Route path="/mypage" element={<Mypage />}></Route>
-        <Route path="/loading" element={<Loading />}></Route>
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <SeoMetaTag
+          title="lymming"
+          description="리밍 | 프로젝트를 시작하기 위한 첫 걸음"
+        />
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/login" element={<LogIn />}></Route>
+          <Route path="/auth" element={<KakaoAuth />}></Route>
+          <Route path="/api/callback" element={<GithubAuth />}></Route>
+          <Route
+            path="/videochat/:roomId"
+            element={<VideoChattingPage />}
+          ></Route>
+          <Route path="/participate" element={<Participate />}></Route>
+          <Route
+            path="/participate/detail/:projectId"
+            element={<ParticipateDetail />}
+          ></Route>
+          <Route path="/teambuild" element={<TeamBuilding />}></Route>
+          <Route path="/chat" element={<ChatPage />}></Route>
+          <Route path="/member" element={<MemberPage />}></Route>
+          <Route path="/share" element={<SharePage />}></Route>
+          <Route path="/share/detail/:id" element={<ShareDetailPage />}></Route>
+          <Route
+            path="/share/detail/leader"
+            element={<ShareDetailLeader />}
+          ></Route>
+          <Route path="/collect" element={<CollectPage />}></Route>
+          <Route path="/mypage" element={<Mypage />}></Route>
+          <Route path="/loading" element={<Loading />}></Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
